bd1c67ce34b4d0546c3a30477855fe1ed1403661
Born-Win <g.l.e.b.b.a.s.66@gmail.com>
2022-10-07 14:13:28 +0300

    Set up app structure and db connection

---
+++ src/exercises/model/index.ts
@@ -0,0 +1,5 @@
+export type Exercise = {
+  id: number;
+  name: string;
+  muscle_subgroup_ids: number[];
+};
---
+++ src/users/users.model.ts
@@ -0,0 +1,13 @@
+import { } from '../results/results.model';
+
+export type User = {
+  id: number;
+  first_name: string;
+  last_name: string;
+
+  // associations
+  // results: {
+
+    
+  // }[]
+}
