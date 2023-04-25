6ee6b91f75468bd2ab724f505287ccad9da11fb3
Born-Win <g.l.e.b.b.a.s.66@gmail.com>
2023-04-25 13:30:33 +0300

    Implemented muscle-groups API

---
+++ migrations/20221013094138-create-and-add-muscle-groups.js
@@ -0,0 +1,58 @@
+const muscleGroupTableName = 'muscle_groups';
+
+const muscleGroups = [
+  {
+    id: 1,
+    name: 'Chest'
+  },
+  {
+    id: 2,
+    name: 'Back'
+  },
+  {
+    id: 3,
+    name: 'Arms'
+  },
+  {
+    id: 4,
+    name: 'Legs'
+  },
+  {
+    id: 5,
+    name: 'Shoulders'
+  },
+  {
+    id: 6,
+    name: 'Abs'
+  }
+];
+
+module.exports = {
+  up: (queryInterface, DataTypes) => {
+    return queryInterface.sequelize.transaction(async transaction => {
+      await queryInterface.createTable(
+        muscleGroupTableName,
+        {
+          id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          name: {
+            type: DataTypes.STRING,
+            allowNull: false
+          }
+        },
+        { transaction }
+      );
+
+      await queryInterface.bulkInsert(muscleGroupTableName, muscleGroups, {
+        transaction
+      });
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable(muscleGroupTableName);
+  }
+};
